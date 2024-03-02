f28672da7255585fc92fd2fbfd245bcf21f09c80
savaleyash004 <159919061+savaleyash004@users.noreply.github.com>
2024-03-02 15:22:59 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,4 @@
+import {a,b,d,de} from "./module2.mjs";
+console.log(a);
+console.log(a);
+console.log(b);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a="kah";
+const b="jhdg";
+const d="kah";
+const de="jhdg";
+// export default a;
+export {a};
+export {b};
+export {d};
+export {de};
